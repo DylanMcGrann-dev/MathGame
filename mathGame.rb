d895769826c3d371362d10172c2ab0b34e524bc7
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-06-21 16:25:33 -0600

    Pseudo code

---
+++ mathGame.rb
@@ -0,0 +1,31 @@
+# What information is relevant to each class?
+# What will they need in order to be initialized?
+# What public methods will be defined on them?
+# Furthermore:
+
+# Which class will contain the game loop (where each instance of the loop is the other players turn)?
+# Which class should manage who the current_player is?
+# Which class(es) will contain user I/O and which will not have any?
+
+#classes 
+  #players
+    # player 1 -- property
+    # player 2 -- property
+  #score
+    # 3/3 -- property
+  #question
+    # generate two numbers between 1 and 20 -- method
+    # what does x plus y equal -- method
+  # Responses
+    # does the calculation -- method
+    # checks if the response is the same -- method
+    # if not return incorrect message and subtract a point and display score -- method
+    # if yes return correct message and display score -- method
+  # messages
+    # stored list of messages
+    # that is correct -- property
+    # that is incorrect -- property
+    # you win -- property
+    # ---new turn---  --property
+    # ---end game---  -- property
+  # turns
